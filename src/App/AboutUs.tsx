8196a281829efc8c9bcb39aa823eea97fe3e44f5
naoki <n.globe.us@gmail.com>
2023-04-11 11:06:21 +0900

    some fix

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -18,14 +18,14 @@ const Content = () => {
           <div className="logo">京田辺マイマップ</div>
         </div>
 
-        <p>京田辺マイマップは、管理人の<a href="https://twitter.com/naogify" target="_blank" rel="noopener noreferrer">naogify</a>が運営する地図サービスです。</p>
+        <p>京田辺マイマップは、管理人の <a href="https://twitter.com/naogify" target="_blank" rel="noopener noreferrer">naogify</a> が運営する地図サービスです。</p>
         <p>京田辺近辺のスポットについて、管理人のコメントと共に紹介します。</p>
         <p>基本的には、子連れで行けるスポットや、飲食店の紹介が多めになると思います。どうぞよろしくお願いします。</p>
-        <p>何かご連絡等あれば、Twitter に DM をお願いします。</p>
+        <p>何かご連絡等あれば、<a href="https://twitter.com/naogify" target="_blank" rel="noopener noreferrer">Twitter</a> に DM をお願いします。</p>
 
         <h2>使用素材</h2>
         <p>このアプリでは、下記の素材を使用しています。</p>
-        <ul>
+        <ul className="materials">
           <li><a href="https://www.city.kyotanabe.lg.jp/0000014375.html" target="_blank" rel="noopener noreferrer">京田辺市文化振興ロゴマーク</a></li>
           <li><a href="https://www.photo-ac.com/main/detail/1453528" target="_blank" rel="noopener noreferrer">写真素材：同志社女子大学 京田辺キャンパス（photoAC Synapse様撮影）</a></li>
         </ul>
